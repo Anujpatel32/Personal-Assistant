d30d6efbf8f3aa1803af8139a05babb19233383a
Anujpatel32 <anujpatel6260@gmail.com>
2026-03-23 10:11:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import random
+import musicLibrary
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def play_random_music():
+    song = random.choice(list(musicLibrary.music.values()))
+    speak("Playing random music")
+    webbrowser.open_new_tab(song)
+
+
+def process_command(command):
+    command = command.lower()
+
+
+    if "open google" in command:
+        speak("Opening Google")
+        webbrowser.open_new_tab("https://www.google.com")
+
+    elif "open youtube" in command:
+        speak("Opening YouTube")
+        webbrowser.open_new_tab("https://www.youtube.com")
+
+    elif "facebook" in command:
+        speak("Opening Facebook")
+        webbrowser.open_new_tab("https://www.facebook.com")
+
+    elif "linkedin" in command:
+        speak("Opening LinkedIn")
+        webbrowser.open_new_tab("https://www.linkedin.com")
+
+
+    elif "play" in command:
+        for key in musicLibrary.music:
+            if key in command:
+                speak(f"Playing {key.lower()} music")
+                webbrowser.open_new_tab(musicLibrary.music[key])
+                return
+
+        play_random_music()
+
+    else:
+        speak("Sorry, I didn't understand that")
+
+
+if __name__ == "__main__":
+    speak("Initializing Mark")
+
+    while True:
+        try:
+
+            with sr.Microphone() as source:
+                print("Waiting for wake word...")
+                audio = recognizer.listen(source, timeout=3, phrase_time_limit=5)
+
+            command = recognizer.recognize_google(audio)
+            print("You said:", command)
+
+
+            if "mark" in command.lower():
+                speak("Yes")
+
+                with sr.Microphone() as source:
+                    print("Listening for command...")
+                    audio = recognizer.listen(source, timeout=3, phrase_time_limit=5)
+
+                command = recognizer.recognize_google(audio)
+                print("Command:", command)
+
+                process_command(command)
+
+        except Exception as e:
+            print("Error:", e)
---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+
+music = {
+    "liked playlist": "https://youtu.be/PtdITNHi68s?si=Q4cF2UWljz586Iay",
+    "old": "https://youtu.be/AMuRRXCuy-4?si=tZyGVtnJNLbbWQ_c",
+    "english": "https://youtu.be/IpFX2vq8HKw?si=qUFEGYCIGQkMdIz6",
+    "refreshing": "https://youtu.be/AYcxiROIktI?si=hycM3Ozd_RrFSCiD"
+}
